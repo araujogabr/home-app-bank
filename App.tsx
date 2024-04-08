0b01b2e61e482e4215b6bf6d7a581a3482402c6d
Gabriel Araújo <gabrielaraujo.professor@gmail.com>
2024-04-08 15:47:15 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import Home from "./screens/Home/Home";
+
+const App = () =>{
+  return(
+    <View>
+      <Home/>
+    </View>
+  );
+}
+
+export default App;
